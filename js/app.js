611ae7c2090905417d61519bcc3ea737bb231dc6
IgorRudenko108 <88731084+IgorRudenko108@users.noreply.github.com>
2022-09-19 16:53:56 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4975,83 +4975,6 @@
                 stop
             });
         }
-        function Grid(_ref) {
-            let {swiper, extendParams} = _ref;
-            extendParams({
-                grid: {
-                    rows: 1,
-                    fill: "column"
-                }
-            });
-            let slidesNumberEvenToRows;
-            let slidesPerRow;
-            let numFullColumns;
-            const initSlides = slidesLength => {
-                const {slidesPerView} = swiper.params;
-                const {rows, fill} = swiper.params.grid;
-                slidesPerRow = slidesNumberEvenToRows / rows;
-                numFullColumns = Math.floor(slidesLength / rows);
-                if (Math.floor(slidesLength / rows) === slidesLength / rows) slidesNumberEvenToRows = slidesLength; else slidesNumberEvenToRows = Math.ceil(slidesLength / rows) * rows;
-                if ("auto" !== slidesPerView && "row" === fill) slidesNumberEvenToRows = Math.max(slidesNumberEvenToRows, slidesPerView * rows);
-            };
-            const updateSlide = (i, slide, slidesLength, getDirectionLabel) => {
-                const {slidesPerGroup, spaceBetween} = swiper.params;
-                const {rows, fill} = swiper.params.grid;
-                let newSlideOrderIndex;
-                let column;
-                let row;
-                if ("row" === fill && slidesPerGroup > 1) {
-                    const groupIndex = Math.floor(i / (slidesPerGroup * rows));
-                    const slideIndexInGroup = i - rows * slidesPerGroup * groupIndex;
-                    const columnsInGroup = 0 === groupIndex ? slidesPerGroup : Math.min(Math.ceil((slidesLength - groupIndex * rows * slidesPerGroup) / rows), slidesPerGroup);
-                    row = Math.floor(slideIndexInGroup / columnsInGroup);
-                    column = slideIndexInGroup - row * columnsInGroup + groupIndex * slidesPerGroup;
-                    newSlideOrderIndex = column + row * slidesNumberEvenToRows / rows;
-                    slide.css({
-                        "-webkit-order": newSlideOrderIndex,
-                        order: newSlideOrderIndex
-                    });
-                } else if ("column" === fill) {
-                    column = Math.floor(i / rows);
-                    row = i - column * rows;
-                    if (column > numFullColumns || column === numFullColumns && row === rows - 1) {
-                        row += 1;
-                        if (row >= rows) {
-                            row = 0;
-                            column += 1;
-                        }
-                    }
-                } else {
-                    row = Math.floor(i / slidesPerRow);
-                    column = i - row * slidesPerRow;
-                }
-                slide.css(getDirectionLabel("margin-top"), 0 !== row ? spaceBetween && `${spaceBetween}px` : "");
-            };
-            const updateWrapperSize = (slideSize, snapGrid, getDirectionLabel) => {
-                const {spaceBetween, centeredSlides, roundLengths} = swiper.params;
-                const {rows} = swiper.params.grid;
-                swiper.virtualSize = (slideSize + spaceBetween) * slidesNumberEvenToRows;
-                swiper.virtualSize = Math.ceil(swiper.virtualSize / rows) - spaceBetween;
-                swiper.$wrapperEl.css({
-                    [getDirectionLabel("width")]: `${swiper.virtualSize + spaceBetween}px`
-                });
-                if (centeredSlides) {
-                    snapGrid.splice(0, snapGrid.length);
-                    const newSlidesGrid = [];
-                    for (let i = 0; i < snapGrid.length; i += 1) {
-                        let slidesGridItem = snapGrid[i];
-                        if (roundLengths) slidesGridItem = Math.floor(slidesGridItem);
-                        if (snapGrid[i] < swiper.virtualSize + snapGrid[0]) newSlidesGrid.push(slidesGridItem);
-                    }
-                    snapGrid.push(...newSlidesGrid);
-                }
-            };
-            swiper.grid = {
-                initSlides,
-                updateSlide,
-                updateWrapperSize
-            };
-        }
         function initSliders() {
             if (document.querySelector(".intro__slider")) new core(".intro__slider", {
                 modules: [ Pagination, Autoplay ],
@@ -5072,42 +4995,6 @@
                     clickable: true
                 },
                 on: {}
-            });
-            if (document.querySelector(".intro-card__slider")) new core(".intro-card__slider", {
-                modules: [ Grid ],
-                observer: true,
-                observeParents: true,
-                slidesPerView: 2,
-                spaceBetween: 20,
-                autoHeight: false,
-                loop: false,
-                breakpoints: {
-                    320: {
-                        slidesPerView: 1.2,
-                        spaceBetween: 20,
-                        grid: false,
-                        centeredSlides: false,
-                        loop: true
-                    },
-                    768: {
-                        slidesPerView: 1.5,
-                        spaceBetween: 20,
-                        grid: false,
-                        centeredSlides: false,
-                        loop: true,
-                        init: true
-                    },
-                    992: {
-                        grid: {
-                            rows: 2,
-                            columns: 2,
-                            fill: "row"
-                        },
-                        slidesPerView: 2,
-                        spaceBetween: 20,
-                        init: false
-                    }
-                }
             });
             if (document.querySelector(".section-category__slider")) new core(".section-category__slider", {
                 observer: true,
