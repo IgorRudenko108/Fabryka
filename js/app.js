fc017ffce28be04ce30080dd29742c7c7dbcff47
IgorRudenko108 <88731084+IgorRudenko108@users.noreply.github.com>
2022-09-23 04:24:38 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -578,6 +578,19 @@
     (() => {
         "use strict";
         const flsModules = {};
+        function isWebp() {
+            function testWebP(callback) {
+                let webP = new Image;
+                webP.onload = webP.onerror = function() {
+                    callback(2 == webP.height);
+                };
+                webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+            }
+            testWebP((function(support) {
+                let className = true === support ? "webp" : "no-webp";
+                document.documentElement.classList.add(className);
+            }));
+        }
         function getHash() {
             if (location.hash) return location.hash.replace("#", "");
         }
@@ -7396,6 +7409,7 @@ PERFORMANCE OF THIS SOFTWARE.
             }));
         }
         window["FLS"] = false;
+        isWebp();
         menuInit();
         spollers();
         tabs();
