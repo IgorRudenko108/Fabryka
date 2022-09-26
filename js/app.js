6b02c619e3eccf8df571d044616296841db33e04
IgorRudenko108 <88731084+IgorRudenko108@users.noreply.github.com>
2022-09-26 10:48:43 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -4829,6 +4829,128 @@
                 loadInSlide
             });
         }
+        function Controller(_ref) {
+            let {swiper, extendParams, on} = _ref;
+            extendParams({
+                controller: {
+                    control: void 0,
+                    inverse: false,
+                    by: "slide"
+                }
+            });
+            swiper.controller = {
+                control: void 0
+            };
+            function LinearSpline(x, y) {
+                const binarySearch = function search() {
+                    let maxIndex;
+                    let minIndex;
+                    let guess;
+                    return (array, val) => {
+                        minIndex = -1;
+                        maxIndex = array.length;
+                        while (maxIndex - minIndex > 1) {
+                            guess = maxIndex + minIndex >> 1;
+                            if (array[guess] <= val) minIndex = guess; else maxIndex = guess;
+                        }
+                        return maxIndex;
+                    };
+                }();
+                this.x = x;
+                this.y = y;
+                this.lastIndex = x.length - 1;
+                let i1;
+                let i3;
+                this.interpolate = function interpolate(x2) {
+                    if (!x2) return 0;
+                    i3 = binarySearch(this.x, x2);
+                    i1 = i3 - 1;
+                    return (x2 - this.x[i1]) * (this.y[i3] - this.y[i1]) / (this.x[i3] - this.x[i1]) + this.y[i1];
+                };
+                return this;
+            }
+            function getInterpolateFunction(c) {
+                if (!swiper.controller.spline) swiper.controller.spline = swiper.params.loop ? new LinearSpline(swiper.slidesGrid, c.slidesGrid) : new LinearSpline(swiper.snapGrid, c.snapGrid);
+            }
+            function setTranslate(_t, byController) {
+                const controlled = swiper.controller.control;
+                let multiplier;
+                let controlledTranslate;
+                const Swiper = swiper.constructor;
+                function setControlledTranslate(c) {
+                    const translate = swiper.rtlTranslate ? -swiper.translate : swiper.translate;
+                    if ("slide" === swiper.params.controller.by) {
+                        getInterpolateFunction(c);
+                        controlledTranslate = -swiper.controller.spline.interpolate(-translate);
+                    }
+                    if (!controlledTranslate || "container" === swiper.params.controller.by) {
+                        multiplier = (c.maxTranslate() - c.minTranslate()) / (swiper.maxTranslate() - swiper.minTranslate());
+                        controlledTranslate = (translate - swiper.minTranslate()) * multiplier + c.minTranslate();
+                    }
+                    if (swiper.params.controller.inverse) controlledTranslate = c.maxTranslate() - controlledTranslate;
+                    c.updateProgress(controlledTranslate);
+                    c.setTranslate(controlledTranslate, swiper);
+                    c.updateActiveIndex();
+                    c.updateSlidesClasses();
+                }
+                if (Array.isArray(controlled)) {
+                    for (let i = 0; i < controlled.length; i += 1) if (controlled[i] !== byController && controlled[i] instanceof Swiper) setControlledTranslate(controlled[i]);
+                } else if (controlled instanceof Swiper && byController !== controlled) setControlledTranslate(controlled);
+            }
+            function setTransition(duration, byController) {
+                const Swiper = swiper.constructor;
+                const controlled = swiper.controller.control;
+                let i;
+                function setControlledTransition(c) {
+                    c.setTransition(duration, swiper);
+                    if (0 !== duration) {
+                        c.transitionStart();
+                        if (c.params.autoHeight) utils_nextTick((() => {
+                            c.updateAutoHeight();
+                        }));
+                        c.$wrapperEl.transitionEnd((() => {
+                            if (!controlled) return;
+                            if (c.params.loop && "slide" === swiper.params.controller.by) c.loopFix();
+                            c.transitionEnd();
+                        }));
+                    }
+                }
+                if (Array.isArray(controlled)) {
+                    for (i = 0; i < controlled.length; i += 1) if (controlled[i] !== byController && controlled[i] instanceof Swiper) setControlledTransition(controlled[i]);
+                } else if (controlled instanceof Swiper && byController !== controlled) setControlledTransition(controlled);
+            }
+            function removeSpline() {
+                if (!swiper.controller.control) return;
+                if (swiper.controller.spline) {
+                    swiper.controller.spline = void 0;
+                    delete swiper.controller.spline;
+                }
+            }
+            on("beforeInit", (() => {
+                swiper.controller.control = swiper.params.controller.control;
+            }));
+            on("update", (() => {
+                removeSpline();
+            }));
+            on("resize", (() => {
+                removeSpline();
+            }));
+            on("observerUpdate", (() => {
+                removeSpline();
+            }));
+            on("setTranslate", ((_s, translate, byController) => {
+                if (!swiper.controller.control) return;
+                swiper.controller.setTranslate(translate, byController);
+            }));
+            on("setTransition", ((_s, duration, byController) => {
+                if (!swiper.controller.control) return;
+                swiper.controller.setTransition(duration, byController);
+            }));
+            Object.assign(swiper.controller, {
+                setTranslate,
+                setTransition
+            });
+        }
         function Autoplay(_ref) {
             let {swiper, extendParams, on, emit} = _ref;
             let timeout;
@@ -5301,6 +5423,49 @@
                     }
                 }
             });
+            $(document).ready((function() {
+                if (document.querySelector(".card-slider")) {
+                    let productSlider = document.querySelectorAll(".card-slider");
+                    let pagination = document.querySelectorAll(".swiper-pagination");
+                    let visualPaginationBullets = document.querySelectorAll(".swiper-pagination-visual");
+                    productSlider.forEach(((slider, index) => {
+                        const swiper = new core(slider, {
+                            modules: [ Pagination, Controller ],
+                            centeredSlides: true,
+                            spaceBetween: 0,
+                            pagination: {
+                                el: pagination[index],
+                                clickable: true
+                            },
+                            breakpoints: {
+                                320: {
+                                    init: false
+                                },
+                                992: {
+                                    init: true
+                                }
+                            },
+                            on: {
+                                init: function() {
+                                    setTimeout((function() {
+                                        $(swiper.$el).find(".swiper-pagination-bullet").hover((function() {
+                                            swiper.slideTo($(this).index());
+                                        }));
+                                    }), 500);
+                                }
+                            }
+                        });
+                        const visualPagination = new core(slider, {
+                            modules: [ Pagination, Controller ],
+                            pagination: {
+                                el: visualPaginationBullets[index],
+                                clickable: false
+                            }
+                        });
+                        swiper.controller.control = visualPagination;
+                    }));
+                }
+            }));
         }
         window.addEventListener("load", (function(e) {
             initSliders();
